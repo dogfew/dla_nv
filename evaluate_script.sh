175998b381e4312259262c5310332b1250f63735
dogfew <volodia.perepyolckin@gmail.com>
2023-12-02 16:24:31 +0300

    url fix

--- evaluate_script.sh
+++ evaluate_script.sh
@@ -2,5 +2,5 @@ pip install -r requirements.txt
 pip install gdown>4.7
 mkdir -p default_test_model
 cd default_test_model
-gdown 1Cv50C8s3Qq54_lndi6AobRQlljUCExLl -O checkpoint.pth
+gdown 1fm1WN9_7TVpzMwYUJwtir6DpPCyQNATm -O checkpoint.pth
 cd ..
